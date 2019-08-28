4a5109cd860f0bc31e547c12793159f107d1e005
MrGio7 <lashagiorgi1233@gmail.com>
2019-08-28 17:31:26 +0400

    Updates

---
+++ precache-manifest.1324e7f752f652a5c55894d61c9f7384.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02516dc213dc3b7b7f0290375e8a2aa1",
+    "url": "/webpage/index.html"
+  },
+  {
+    "revision": "5f4b4e0ed32c94bad281",
+    "url": "/webpage/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "86e4218132d298fe7139",
+    "url": "/webpage/static/css/main.c3a27772.chunk.css"
+  },
+  {
+    "revision": "5f4b4e0ed32c94bad281",
+    "url": "/webpage/static/js/2.a00c677b.chunk.js"
+  },
+  {
+    "revision": "86e4218132d298fe7139",
+    "url": "/webpage/static/js/main.af12924d.chunk.js"
+  },
+  {
+    "revision": "5cd048196658627e555a",
+    "url": "/webpage/static/js/runtime~main.2b22c7c2.js"
+  },
+  {
+    "revision": "50afd386e1cbbcbf200cb06c713f9fa2",
+    "url": "/webpage/static/media/background.50afd386.jpg"
+  },
+  {
+    "revision": "641a933347ce4318fb4adf5c71b265fe",
+    "url": "/webpage/static/media/header1.641a9333.png"
+  },
+  {
+    "revision": "3f6967a31c8bbdee5953785036824a56",
+    "url": "/webpage/static/media/header2.3f6967a3.png"
+  }
+]);
